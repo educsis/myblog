18de6a5c655d9fca51837ad0e690b885244e7265
Edwin Orellana <educsis@gmail.com>
2024-10-05 23:19:10 -0600

    session 3

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
+const Card = require('./src/_includes/components/Card')
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/");
     eleventyConfig.addPassthroughCopy("src/css/");
     eleventyConfig.addWatchTarget("src/css/");
+    eleventyConfig.addShortcode("Card", Card)
     return {
             dir: {
                 input: 'src',
